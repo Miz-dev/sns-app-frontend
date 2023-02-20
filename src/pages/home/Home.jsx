4e7cabef89605bddeca59a07f39463917b1bf079
Miz-dev <mizlog.dev@gmail.com>
2023-02-20 20:38:21 +0900

    ✨ Sidebarコンポーネント作成

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Sidebar from "../../components/Topbar/Sidebar/Sidebar";
 import Topbar from "../../components/Topbar/Topbar";
 
 export default function Home() {
 	return (
 		<>
 			<Topbar />
+			<Sidebar/>
 		</>
 	);
 }
