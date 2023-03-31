8f4f9541122901a59752e727ab4f4d1935fe8a12
Miz-dev <mizlog.dev@gmail.com>
2023-03-31 21:41:38 +0900

    💄 Postコンポーネントスタイリング

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -6,24 +6,32 @@ export default function Post() {
   return (
     <div className="post">
       <div className="postWrapper">
-        <img src="./assets/person/1.png" alt="" className="postProfileImg" />
-        <span className="postUserName">Miz</span>
-        <span className="postDate">5分前</span>
-      </div>
-      <div className="postRight">
-        <MoreVert />
-      </div>
-      <div className="postCenter">
-        <span className="postText">SNSを自作中です。</span>
-        <img src="./assets/post/1.jpeg" alt="" className="postImg" />
-      </div>
-      <div className="postBottom">
-        <div className="postBottomLeft">
-          <img src="./assets/heart.png" alt="" className="likeIcon" />
-          <span className="postLikeCounter">5人がいいねを押しました。</span>
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img
+              src="./assets/person/1.png"
+              alt=""
+              className="postProfileImg"
+            />
+            <span className="postUserName">Miz</span>
+            <span className="postDate">5分前</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
         </div>
-        <div className="postBottomRight">
-          <span className="postCommentText">4:コメント</span>
+        <div className="postCenter">
+          <span className="postText">SNSを自作中です。</span>
+          <img src="./assets/post/1.jpeg" alt="" className="postImg" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img src="./assets/heart.png" alt="" className="likeIcon" />
+            <span className="postLikeCounter">5人がいいねを押しました。</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">4:コメント</span>
+          </div>
         </div>
       </div>
     </div>
