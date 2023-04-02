417fa3150fcd59bf45dedca7012b35fdd9f12f4c
Miz-dev <mizlog.dev@gmail.com>
2023-04-02 20:59:05 +0900

    ✨ Rightbarコンポーネント雛形作成

--- src/components/Rightbar/Rightbar.jsx
+++ src/components/Rightbar/Rightbar.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
-import "./Rightbar.css"
+import React from "react";
+import "./Rightbar.css";
 
 export default function Rightbar() {
-    return (
-        <div className='rightbar'>Rightbar</div>
-    )
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="eventContainer">
+          <img src="assets/star.png" alt="" className="starImg" />
+          <span className="eventText">
+            <b>フォロワー限定</b>イベント開催中！
+          </span>
+        </div>
+        <img src="assets/event.jpeg" alt="" />
+        <h4 className="rightbarTitle">オンラインの友達</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="assets/person/1.jpeg" alt="" />
+              <span className="rightbarUsername">Shin code</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
